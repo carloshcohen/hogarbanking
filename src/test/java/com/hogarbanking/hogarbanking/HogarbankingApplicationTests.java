1ca1648d1b912a4b4eb90f5476fc1272a6cae392
Carlos <carloshcohen@hotmail.com>
2023-05-18 11:35:51 -0300

    first commit

---
+++ src/test/java/com/hogarbanking/hogarbanking/HogarbankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hogarbanking.hogarbanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogarbankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
